999f8b9d94ed7535dcb207ec6776a0bc6c939ef2
chopsticks35 <tiwegmeyeryoga@gmail.com>
2016-01-18 17:09:44 -0700

    changing to add angular js library

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ var inputNumber;
 var name;
 var phoneNumber;
 var streetAddress;
-var searchArray
-var person
+var searchArray;
+var person;
 
-function searchNumber() {
-
-    inputNumber = window.prompt("How many disaster victims would you like to search?");
-    searchArray = new Array[inputNumber];
-}
-
-function personObject() {
-    for (var i=1; i<=searchArray.length; i++) {
-        person = window.display("Enter the name of person #" + i);
-        
-    }
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.name="Jane Doe";
     
-}
+});
 
-searchNumber();
-personObject();
+//function searchNumber() {
+//
+//    inputNumber = window.prompt("How many disaster victims would you like to search?");
+//    searchArray = new Array[inputNumber];
+//}
+//
+//function personObject() {
+//    for (var i=1; i<=searchArray.length; i++) {
+//        person = window.display("Enter the name of person #" + i);
+//        
+//    }
+//    
+//}
+//
